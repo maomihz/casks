9617fc9cbb58eaa941991e40e3c1a70149f8499f
MaomiHz <maomihz@gmail.com>
2016-06-03 19:08:01 +0800

    add bbb.app

---
+++ Casks/bbb.app.rb
@@ -0,0 +1,11 @@
+cask 'bbb.app' do
+	version 'a0.1'
+	sha256 '9944c270365a209e0d205573c99291e0867852bedfe8392782cc6d003973f724'
+	
+	url "https://lk1.bid/bbb-#{version}.zip"
+	name 'bbb.app! by SeanGame'
+	homepage 'https://github.com/seancheey/'
+	license :freemium
+	
+	app 'bbb.app'
+end

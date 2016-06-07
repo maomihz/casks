474009486734faccb0ce1a48125456e8fc76ed5e
MaomiHz <maomihz@gmail.com>
2016-06-07 09:44:57 +0800

    update bbb.app

--- Casks/bbb.app.rb
+++ Casks/bbb.app.rb
@@ -1,7 +1,7 @@
 cask 'bbb.app' do
-	version 'a0.1'
-	sha256 '9944c270365a209e0d205573c99291e0867852bedfe8392782cc6d003973f724'
-	
+	version 'a0.2'
+	sha256 'e9b176daa68b3f22c2b97f5376e328f583c7bdbc0ba417d4ff816c5db914f375'
+
 	url "https://lk1.bid/bbb-#{version}.zip"
 	name 'bbb.app! by SeanGame'
 	homepage 'https://github.com/seancheey/'

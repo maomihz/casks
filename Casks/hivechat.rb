51e2c72c2c2c2733fca187efd44eab788b1d631c
MaomiHz <maomihz@gmail.com>
2016-12-03 14:19:57 -0600

    update Hive! 0.0.7.2

--- Casks/hivechat.rb
+++ Casks/hivechat.rb
@@ -1,11 +1,17 @@
 cask 'hivechat' do
-	version '0.0.4'
-	sha256 '256b5f70d5195298fc4b65c83c2fa30395be387f0e1abbd4dc14d4df090bb632'
-	
-	url "http://7xpoh1.dl1.z0.glb.clouddn.com/Hive_#{version}_alpha_osx_new_Qt5.6.zip.dmg"
+	version '0.0.7.2'
+
+	if MacOS.version <= '10.8'
+		url "http://ohc054eha.bkt.gdipper.com/Hive_#{version}_alpha_osx_old_Qt5.5.1.dmg"
+		sha256 '834e2bf644ba54668b69462dff7b8455c35e3ff5fdee53f1e6cd6f1'
+	else
+		url "http://ohc054eha.bkt.gdipper.com/Hive_#{version}_alpha_osx_new_Qt5.7.0.dmg"
+		sha256 'd5620c815414cc82be850f9fbe9d91bb71ef255aa0267011d07577332d4b4038'
+	end
+
 	name 'Hive! Chat'
 	homepage 'http://hivechat.org'
 	license :gpl
-	
+
 	app 'Hive!.app'
 end
